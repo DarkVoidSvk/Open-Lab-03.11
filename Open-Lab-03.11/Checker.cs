7604ab920a7c1d04841701826f72018800007595
DarkVoidSvk <samueljakubecsk@gmail.com>
2021-09-25 10:24:23 +0200

    Implemented method that checks if a string is a palindrome.

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -4,9 +4,24 @@ namespace Open_Lab_03._11
 {
     public class Checker
     {
+        static string Reverse_String(string To_Be_Reversed)
+        {
+            char[] characters = To_Be_Reversed.ToCharArray();
+            Array.Reverse(characters);
+            return new string(characters);
+        }
+
+
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
+            if (str == Reverse_String(str))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         } 
     }
 }
